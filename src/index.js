3ce605cde95da126babed27777ae98bf6672422e
Kanat Juzbayev <76990478+KanatJuzbayev@users.noreply.github.com>
2021-03-12 21:24:50 +0600

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
-  return 0;
+  if ( !array || array.length == 0) {
+    return 0;
+} 
+  const minValue = Math.min(...array)
+  return minValue;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if ( !array || array.length == 0) {
+    return 0;
+} 
+  const maxValue = Math.max(...array)
+  return maxValue;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if ( !array || array.length == 0) {
+    return 0;
+} 
+  var sum = array.reduce(function(a, b) {
+        return a + b;
+    }, 0);
+  return sum/array.length ;
 }
